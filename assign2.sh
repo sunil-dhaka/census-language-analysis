915bd0758066ae6f00512aba4c6380200a07c6cb
elliot <dhaka.sonu.monu@gmail.com>
2021-11-09 23:38:16 +0530

    add top level shell script for the project

---
+++ assign2.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+bash percent-india.sh
+bash gender-india.sh
+bash geography-india.sh
+bash 3-to-2-ratio.sh
+bash 2-to-1-ratio.sh
+bash age-india.sh
+bash literacy-india.sh
+bash region-india.sh
+bash age-gender.sh
+bash literacy-gender.sh
